7daddfe404483d2712564adc122b17361f64cf3b
EDWARD <E0003394@u.nus.edu>
2017-02-18 11:38:31 +0800

    fix more bugs

--- src/seedu/addressbook/data/person/Person.java
+++ src/seedu/addressbook/data/person/Person.java
@@ -10,7 +10,7 @@ import java.util.Objects;
  */
 public class Person implements ReadOnlyPerson {
 
-	private static int nextSequenceNumber=0;
+	private static int nextSequenceNumber=1;
 	private int sequenceNumber;
 	
     private Name name;
@@ -28,8 +28,8 @@ public class Person implements ReadOnlyPerson {
         this.email = email;
         this.address = address;
         this.tags = new UniqueTagList(tags); // protect internal tags from changes in the arg list
-        nextSequenceNumber++;
         this.sequenceNumber= nextSequenceNumber;
+        nextSequenceNumber++;
     }
 
     /**
