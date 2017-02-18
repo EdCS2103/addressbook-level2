0450eca0d6959b9e7b60d54e3a551ea130af35a9
EDWARD <E0003394@u.nus.edu>
2017-02-19 01:42:13 +0800

    create testIsSimilar test case

---
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -0,0 +1,16 @@
+package seedu.addressbook.data.person;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NameTest {
+/**
+ * test for method isSimilar in Name Class
+ */
+	@Test
+	public void testIsSimilar(){
+		fail("Not yet implemented");
+	}
+
+}
