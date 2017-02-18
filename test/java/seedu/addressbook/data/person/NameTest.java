56b2f292b0401f7e110b7fc6d281da32d72d624b
EDWARD <E0003394@u.nus.edu>
2017-02-19 03:24:06 +0800

    refactor and SLAP

--- test/java/seedu/addressbook/data/person/NameTest.java
+++ test/java/seedu/addressbook/data/person/NameTest.java
@@ -27,6 +27,46 @@ public class NameTest {
 		assertTrue(handsome.isSimilar(diffCase));
 		assertTrue(handsome.isSimilar(superset));
 		assertFalse(handsome.isSimilar(nullset));
+		
+		testSameFullName();
+		testDiffCase();
+		testSuperOrSubSet();
+		testNullSet();
 	}
+
+private void testNullSet() throws IllegalValueException {
+	Name handsome = new Name("henry");
+	Name nullset = null;
+	assertFalse(handsome.isSimilar(nullset));
+}
+
+private void testSuperOrSubSet() throws IllegalValueException {
+	Name handsome = new Name("henry");
+	Name superset1 = new Name("Henry Cheese");
+	Name superset2 = new Name("henry Cheesecake");
+	assertTrue(superset1.isSimilar(handsome));
+	assertTrue(handsome.isSimilar(superset1));
+	assertTrue(handsome.isSimilar(superset2));
+	assertTrue(superset2.isSimilar(handsome));
+}
+
+private void testDiffCase() throws IllegalValueException {
+	Name handsome = new Name("henry");
+	Name diffCase = new Name("HenRy");
+	assertTrue(handsome.isSimilar(diffCase));
+	assertTrue(diffCase.isSimilar(handsome));
+}
+
+private void testSameFullName() throws IllegalValueException {
+	Name handsome = new Name("henry");
+	Name pretty = new Name("chia");
+	Name handsome2 = new Name("henry");
+	
+	assertFalse(handsome.isSimilar(pretty));
+	assertFalse(pretty.isSimilar(handsome));
+	assertTrue(handsome.isSimilar(handsome));
+	assertTrue(handsome.isSimilar(handsome2));
+	assertTrue(handsome2.isSimilar(handsome));
+}
 
 }
